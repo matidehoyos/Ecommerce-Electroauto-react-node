3f43a87370c85f0a78a3cedfcf049b9f764bcfc3
Matias <mati.dehoyosmdp@gmail.com>
2024-03-25 13:21:05 -0300

    deploy

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ server.use(express.json());
 server.use(morgan("dev"));
 server.use(cors());
 server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', 'https://electrocar-li1ov0w8u-matidehoyos-projects.vercel.app');
+   res.header('Access-Control-Allow-Origin', 'https://electroauto.vercel.app');
    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
    res.header('Access-Control-Allow-Headers', 'Content-Type'); 
    res.header('Access-Control-Allow-Credentials', true); // Habilitar credenciales
