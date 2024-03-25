76f31de2ea49eca3ccf4e6382c00ba120e1d33b2
Matias <mati.dehoyosmdp@gmail.com>
2024-03-25 12:21:26 -0300

    dsfdfs

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(cors());
 server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Origin', 'https://electrocar.vercel.app/');
    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
    res.header('Access-Control-Allow-Headers', 'Content-Type'); 
    res.header('Access-Control-Allow-Credentials', true); // Habilitar credenciales
