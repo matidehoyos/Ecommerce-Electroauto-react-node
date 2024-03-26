5c81700c17e6b45d6880f711b75829e3f370db39
Matias <mati.dehoyosmdp@gmail.com>
2024-03-26 12:04:35 -0300

    dfs

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ server.use(express.json());
 server.use(morgan("dev"));
 server.use(cors());
 server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', 'http://electroauto.vercel.app');
+   res.header('Access-Control-Allow-Origin', 'https://electroauto.vercel.app');
    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
    res.header('Access-Control-Allow-Headers', 'Content-Type'); 
    res.header('Access-Control-Allow-Credentials', true); // Habilitar credenciales
