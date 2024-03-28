bd0587ae22f0b2682bf77ddc21ab52a34ac2dc08
Matias <mati.dehoyosmdp@gmail.com>
2024-03-28 12:56:56 -0300

    resp

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,8 +14,8 @@ import axios from "axios";
 import CarritoContext from './components/carritoContext/CarritoContext';
 import { useState } from 'react';
 
-  // axios.defaults.baseURL = 'https://electrocar-production.up.railway.app'
-  axios.defaults.baseURL = 'http://localhost:3000'
+   axios.defaults.baseURL = 'https://electrocar-production.up.railway.app'
+  // axios.defaults.baseURL = 'http://localhost:3000'
 
 function App() {
   const [carrito, setCarrito] = useState(JSON.parse(localStorage.getItem('carrito')) || []);
