a022d688a1ec293d0748b372e6cf259c9aecc0f0
Matias <mati.dehoyosmdp@gmail.com>
2024-03-28 11:26:15 -0300

    cambios

--- Frontend/src/components/productoTiendaCard/ProductoTiendaCard.jsx
+++ Frontend/src/components/productoTiendaCard/ProductoTiendaCard.jsx
@@ -1,13 +1,13 @@
 import { useContext, useState } from "react";
 import style from "./ProductoTiendaCard.module.css";
 import { FaShoppingCart } from 'react-icons/fa';
-import { Link } from "react-router-dom";
+import Modal from 'react-modal';
 import CarritoContext from "../carritoContext/CarritoContext";
-
 
 const ProductoTiendaCard = ({producto}) => {
   const { carrito, setCarrito } = useContext(CarritoContext);
   const [unidades, setUnidades] = useState(1);
+  const [modalIsOpen, setModalIsOpen] = useState(false); 
   const subtotal = unidades * producto.precio;
 
   const handleChange = (e) => {
@@ -33,13 +33,11 @@ const ProductoTiendaCard = ({producto}) => {
     setCarrito(carrito);
   }
 
-
-      const nombreProducto = producto?.name.toUpperCase();
+  const nombreProducto = producto?.name.toUpperCase();
 
   return (
-    
       <div className={style.container}>
-        <Link to={`/producto/${producto.id}`} className={style.link}>
+        <div onClick={() => setModalIsOpen(true)} className={style.link}> 
           <div className={style.imgContainer}>
             <img src={producto.imagen} alt={producto.name}/>
           </div>   
@@ -48,20 +46,30 @@ const ProductoTiendaCard = ({producto}) => {
           <p className={style.detalle}>{producto.detalle}</p>    
           <p className={style.stock}>Stock disponible: <span>{producto.cantidad}</span></p>
           <p className={style.precio}><span className={style.precioNumero}>${producto.precio}</span></p> 
-        </Link>    
-          <div className={style.botonAgregar}>
-            <div className={style.unidades}>
-              <label>Cantidad:</label>
-              <select onChange={handleChange}>
-            {[...Array(20).keys()].map((value, index) => 
-          <option key={index} value={value + 1}>
-            {value + 1} {value === 0 ? 'unidad' : 'unidades'}
-          </option>
-        )}
-            </select> 
-            </div>
-              <button className={style.botonKart} onClick={agregarAlCarrito}><FaShoppingCart className={style.icon}/>AGREGAR AL CARRITO</button>  
-          </div>  
+        </div>    
+        <div className={style.botonAgregar}>
+          <div className={style.unidades}>
+            <label>Cantidad:</label>
+            <select onChange={handleChange}>
+          {[...Array(20).keys()].map((value, index) => 
+        <option key={index} value={value + 1}>
+          {value + 1} {value === 0 ? 'unidad' : 'unidades'}
+        </option>
+      )}
+          </select> 
+          </div>
+            <button className={style.botonKart} onClick={agregarAlCarrito}><FaShoppingCart className={style.icon}/>AGREGAR AL CARRITO</button>  
+        </div>  
+
+        <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+          <button onClick={() => setModalIsOpen(false)}>X</button>
+          <img src={producto.imagen} alt={producto.name}/>
+          <h2>{producto.name}</h2>
+          <p>{producto.detalle}</p>
+          <p>{producto.informacion}</p>
+          <p>${producto.precio}</p>
+          <button onClick={agregarAlCarrito}>Agregar al carrito</button>
+        </Modal>
       </div>
   );
 };
