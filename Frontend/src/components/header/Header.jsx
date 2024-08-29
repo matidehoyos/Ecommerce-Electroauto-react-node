4ecc02040afd0ee44066d417bae2888fa179eb55
Matias <mati.dehoyosmdp@gmail.com>
2024-08-29 18:34:44 +0100

    responsive

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -12,39 +12,9 @@ const Header = () => {
           <img src='logoPc.png' alt="logo" />
         </div>
         <div className={style.texto}>
-          <span>Somos</span>
-            <h1 className={style.textoEfecto}>
-              <span style={{'--i': 1}}>E</span><span style={{'--i': 2}}>L</span><span style={{'--i': 3}}>E</span><span style={{'--i': 4}}>C</span><span style={{'--i': 5}}>T</span><span style={{'--i': 6}}>R</span><span style={{'--i': 7}}>O</span><span style={{'--i': 7}}>A</span><span style={{'--i': 8}}>U</span><span style={{'--i': 9}}>T</span><span style={{'--i': 10}}>O</span>
-              </h1>
-            <h5>
-            <span>A</span>
-  <span>C</span>
-  <span>C</span>
-  <span>E</span>
-  <span>S</span>
-  <span>O</span>
-  <span>R</span>
-  <span>I</span>
-  <span>O</span>
-  <span>S</span>
-  <span> </span>
-  <span>P</span>
-  <span>A</span>
-  <span>R</span>
-  <span>A</span>
-  <span> </span>
-  <span>T</span>
-  <span>U</span>
-  <span> </span>
-  <span>V</span>
-  <span>E</span>
-  <span>H</span>
-  <span>I</span>
-  <span>C</span>
-  <span>U</span>
-  <span>L</span>
-  <span>O</span>
-            </h5>
+          <span className={style.somos}>Somos</span>
+            <h1 className={style.textoEfecto}><span style={{'--i': 1}}>E</span><span style={{'--i': 2}}>L</span><span style={{'--i': 3}}>E</span><span style={{'--i': 4}}>C</span><span style={{'--i': 5}}>T</span><span style={{'--i': 6}}>R</span><span style={{'--i': 7}}>O</span><span style={{'--i': 7}}>A</span><span style={{'--i': 8}}>U</span><span style={{'--i': 9}}>T</span><span style={{'--i': 10}}>O</span></h1>
+            <h5>ACCESORIOS PARA TU VEHICULO</h5>
             <div className={style.envios}>
               <h6>ENVIOS A TODO EL PAÍS</h6>
               <h6 className={style.mayor}>MAYORISTA Y MINORISTA</h6>
--- Frontend/src/components/infoEnvios/InfoEnvios.jsx
+++ Frontend/src/components/infoEnvios/InfoEnvios.jsx
@@ -9,9 +9,13 @@ const InfoEnvios = () => {
           <div className={style.text}>
             <p><FaTruck /></p>
             <div className={style.first}>
-            <h4>ENVIO GRATIS A TODO EL PAÍS</h4>
-            <h5>CON TU COMPRA MAYOR A $90.000</h5>
-          </div>
+              <h4>ENVIO GRATIS A TODO EL PAÍS</h4>
+              <h5>CON TU COMPRA MAYOR A $90.000</h5>
+            </div>
+            <div className={style.firstMovil}>
+              <h4>ENVIO GRATIS<br/>A TODO EL PAÍS</h4>
+              <h5>CON TU COMPRA<br/>MAYOR A $90.000</h5>
+            </div>
         </div>
     </div>
   );
